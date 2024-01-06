91abcbc87cd3d1ae42f55377ba3cef209b40d693
John Rodrigues <john.rodrigues008@gmail.com>
2024-01-06 20:51:34 -0300

    Atualização da pagina de contatos
    Signed-off-by: John Rodrigues <john.rodrigues008@gmail.com>

--- src/components/sections/FooterSection.tsx
+++ src/components/sections/FooterSection.tsx
@@ -1,6 +1,6 @@
 export function FooterSection() {
   return (
-    <footer className="w-full mt-28 py-8 px-4 md:px-8 border-t border-t-zinc-700">
+    <footer className="w-full mt-20 py-8 px-4 md:px-8 border-t border-t-zinc-700">
       
         <div className="container flex flex-col justify-center items-center max-w-7xl mx-auto px-4 md:px-8 sm:flex-row sm:justify-between">
           <p className="text-zinc-500 text-sm">
